cae6f1436990b001435a02f2283588f7e2305278
regnat-official <mega.tanki007@gmail.com>
2024-10-09 15:02:53 +0300

    Create animeData.js

---
+++ animeData.js
@@ -0,0 +1,21 @@
+module.exports = [
+    {
+        title: "Naruto",
+        description: "История о ниндзя.",
+        image: "/images/naruto.jpg",
+        popularity: 9.8
+    },
+    {
+        title: "One Piece",
+        description: "Путешествие пиратов за сокровищем.",
+        image: "/images/onepiece.jpg",
+        popularity: 9.7
+    },
+    {
+        title: "Attack on Titan",
+        description: "Борьба человечества с титанами.",
+        image: "/images/aot.jpg",
+        popularity: 9.5
+    }
+    // Добавьте больше аниме по желанию
+];
